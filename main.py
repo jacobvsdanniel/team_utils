9c4f8082abe9fc68e124881216c050116455669c
Peng-Hsuan Li <jacobvsdanniel@gmail.com>
2021-06-16 15:38:13 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import itertools
 
 
 class Player:
-    def __init__(self, name, pocket, flank):
+    def __init__(self, name, pocket_rating, flank_rating):
         self.name = name
-        self._pocket = pocket
-        self._flank = flank
+        self.pocket_rating = pocket_rating
+        self.flank_rating = flank_rating
         return
 
-    def flank(self):
-        assert self._flank >= 0
-        return self._flank
-
     def pocket(self):
-        assert self._pocket >= 0
-        return self._pocket
+        assert self.pocket_rating >= 0
+        return self.pocket_rating
+
+    def flank(self):
+        assert self.flank_rating >= 0
+        return self.flank_rating
 
 
 class Team:
@@ -24,23 +24,23 @@ class Team:
         self.player_list = player_list
 
         if len(player_list) == 1:
-            score_l = player_list[0].flank()
-            score_r = player_list[0].flank()
+            score_w = player_list[0].flank()
+            score_e = player_list[0].flank()
             score_t = player_list[0].flank()
         else:
             n = math.floor(len(player_list) / 2)
-            score_l = player_list[0].flank()
+            score_w = player_list[0].flank()
             for player in player_list[1:n]:
-                score_l += player.pocket()
-            score_r = player_list[-1].flank()
+                score_w += player.pocket()
+            score_e = player_list[-1].flank()
             for player in player_list[-n:-1]:
-                score_r += player.pocket()
-            score_t = score_l + score_r
+                score_e += player.pocket()
+            score_t = score_w + score_e
             if len(player_list) % 2 == 1:
                 score_t += player_list[n].pocket()
 
-        self.score_l = score_l
-        self.score_r = score_r
+        self.score_w = score_w
+        self.score_e = score_e
         self.score_t = score_t
         return
 
@@ -64,8 +64,8 @@ def team_up(player_list):
             team1, team2 = Team(sorted_player_list[:team_size]), Team(sorted_player_list[team_size:])
         except AssertionError:
             continue
-        l_diff = abs(team1.score_l - team2.score_l)
-        r_diff = abs(team1.score_r - team2.score_r)
+        l_diff = abs(team1.score_w - team2.score_w)
+        r_diff = abs(team1.score_e - team2.score_e)
         t_diff = abs(team1.score_t - team2.score_t)
         if len(player_list) % 4 == 0:
             score_diff = max(l_diff, r_diff, t_diff)
@@ -75,11 +75,13 @@ def team_up(player_list):
             best_team_up = (team1, team2, score_diff)
 
     team1, team2, score_diff = best_team_up
+    print("[Details]")
     team1.show(prefix="team1: ")
     team2.show(prefix="team2: ")
-    print(f" left wing, team1-team2={team1.score_l - team2.score_l}")
-    print(f"right wing, team1-team2={team1.score_r - team2.score_r}")
-    print(f"whole team, team1-team2={team1.score_t - team2.score_t}")
+    print(f" west wing: team1-team2={team1.score_w - team2.score_w}")
+    print(f" east wing: team1-team2={team1.score_e - team2.score_e}")
+    print(f"whole team: team1-team2={team1.score_t - team2.score_t}")
+    print("\n[Result]")
     team1.show(casual=True)
     team2.show(casual=True)
     return
@@ -87,16 +89,16 @@ def team_up(player_list):
 
 def main():
     player_list = [
-        Player("THD", 100,  90),
-        Player("HBT",  -1,  80),
-        Player("TTL",  80,  -1),
-        Player("JCB",  -1,  60),
-        Player("R04",  60,  50),
-        Player("EFO",  -1,  55),
-        # Player("HNH",  55,  -1),
-        # Player("ZNN",  40,  40),
-        # Player("SCR",  35,  -1),
-        # Player("EAI",  60,  60),
+        Player("THD", pocket_rating=100, flank_rating= 90),
+        Player("HBT", pocket_rating= -1, flank_rating= 80),
+        Player("TTL", pocket_rating= 80, flank_rating= -1),
+        Player("JCB", pocket_rating= -1, flank_rating= 60),
+        Player("R04", pocket_rating= 60, flank_rating= 50),
+        Player("EFO", pocket_rating= -1, flank_rating= 55),
+        Player("HNH", pocket_rating= 55, flank_rating= -1),
+        Player("ZNN", pocket_rating= 40, flank_rating= 40),
+        # Player("SCR", pocket_rating= 35, flank_rating= -1),
+        # Player("EAI", pocket_rating= 60, flank_rating= 60),
     ]
     team_up(player_list)
     return
